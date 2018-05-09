08577fd4b7714baa58ab8355e12809b45fd7fae9
Edison Neto <ednetoali@gmail.com>
2018-05-09 17:30:06 -0300

    modified:   BinaryConverter.java
    Enhanced the code quality.

--- BinaryConverter.java
+++ BinaryConverter.java
@@ -1,32 +1,47 @@
 import java.util.Scanner;
 
-public class BinaryConverter {
-    public static String reverseString(String number) {
-        String newNumber = new StringBuilder(number).reverse().toString();
-        return newNumber;
+class IntegerBinaryConverter {
+    private String binaryNumber;
+    public double finalNumber;
+
+    public IntegerBinaryConverter(String binaryNumber) {
+        this.binaryNumber = binaryNumber;
     }
 
-    public static double toDecimal(String number) {
+    public void setBinaryNumber(String binaryNumber) {
+        this.binaryNumber = binaryNumber;
+    }
+
+    public void reverseString() {
+        String newNumber = new StringBuilder(this.binaryNumber).reverse().toString();
+        setBinaryNumber(newNumber);
+    }
+
+    public double toDecimal() {
         int counter = 0;
         double finalSum = 0;
-        for (char singleNumber : number.toCharArray()) {
+        for (char singleNumber : this.binaryNumber.toCharArray()) {
             int realNumber = Character.getNumericValue(singleNumber);
             double math = realNumber * Math.pow(2, counter);
             finalSum += math;
             counter++;
         }
-        return finalSum;
+        this.finalNumber = finalSum;
+        return this.finalNumber;
     }
+}
 
+public class BinaryConverter {
     public static void main(String[] args) {
         Scanner binaryNumber = new Scanner(System.in);
         System.out.println("Your binary number: ");
         String number = binaryNumber.nextLine();
-        // Reverts the number.
-        String newNumber = reverseString(number);
-        System.out.println(newNumber);
 
-        double finalNumber = toDecimal(newNumber);
+        // Reverts the number.
+        IntegerBinaryConverter converter = new IntegerBinaryConverter(number);
+        converter.reverseString();
+
+        double finalNumber = converter.toDecimal();
         System.out.println("Your binary number in decimal is: " + finalNumber);
 
         binaryNumber.close();
