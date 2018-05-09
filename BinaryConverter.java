78827be2f84d982f62b31941c7f8bb6b134089d4
Edison Neto <ednetoali@gmail.com>
2018-05-09 01:45:20 -0300

    modified:   BinaryConverter.java
    I finally did the binary converter. It only works with integers now,
    tomorrow i'll make work with floats or doubles.

--- BinaryConverter.java
+++ BinaryConverter.java
@@ -2,20 +2,32 @@ import java.util.Scanner;
 
 public class BinaryConverter {
     public static String reverseString(String number) {
-        for (int i = 0;i < number.length(); i++) {
-            System.out.println(i);
+        String newNumber = new StringBuilder(number).reverse().toString();
+        return newNumber;
+    }
+
+    public static double toDecimal(String number) {
+        int counter = 0;
+        double finalSum = 0;
+        for (char singleNumber : number.toCharArray()) {
+            int realNumber = Character.getNumericValue(singleNumber);
+            double math = realNumber * Math.pow(2, counter);
+            finalSum += math;
+            counter++;
         }
-        return "a";
+        return finalSum;
     }
 
     public static void main(String[] args) {
         Scanner binaryNumber = new Scanner(System.in);
-        System.out.println("Your binary number: " + binaryNumber);
+        System.out.println("Your binary number: ");
         String number = binaryNumber.nextLine();
+        // Reverts the number.
+        String newNumber = reverseString(number);
+        System.out.println(newNumber);
 
-        System.out.println(number);
-
-        reverseString(number);
+        double finalNumber = toDecimal(newNumber);
+        System.out.println("Your binary number in decimal is: " + finalNumber);
 
         binaryNumber.close();
     }
