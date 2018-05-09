549bf6466f558beb336f8f8532f7c9cad04762cb
Edison Neto <ednetoali@gmail.com>
2018-05-09 17:49:22 -0300

    modified:   BinaryConverter.java
    Added the float (double) conversion, now i need to implement the testing
    to know if the number is a integer or float.

--- BinaryConverter.java
+++ BinaryConverter.java
@@ -1,5 +1,52 @@
 import java.util.Scanner;
 
+class doubleBinaryConverter {
+    private String binaryNumber;
+    public double finalNumber = 0;
+    private String floatPart;
+    private String integerPart;
+
+    public doubleBinaryConverter(String binaryNumber) {
+        this.binaryNumber = binaryNumber;
+    }
+
+    public void setBinaryNumber(String binaryNumber) {
+        this.binaryNumber = binaryNumber;
+    }
+
+    public void revertString() {
+        String newNumber = new StringBuilder(this.binaryNumber).reverse().toString();
+        setBinaryNumber(newNumber);
+    }
+
+    public void splitNumber() {
+        String integerPart = this.binaryNumber.split("\\.")[1];
+        String floatPart = this.binaryNumber.split("\\.")[0];
+        this.integerPart = integerPart;
+        this.floatPart = floatPart;
+    }
+
+    public double convertNumber() {
+        int counter = 0 - this.floatPart.length();
+
+        for (char singleNumber : this.floatPart.toCharArray()) {
+            double realNumber = Character.getNumericValue(singleNumber);
+            double math = realNumber * Math.pow(2, counter);
+            this.finalNumber += math;
+            counter++;
+        }
+        counter = 0;
+        for (char singleNumber : this.integerPart.toCharArray()) {
+            double realNumber = Character.getNumericValue(singleNumber);
+            double math = realNumber * Math.pow(2, counter);
+            this.finalNumber += math;
+            counter ++;
+        }
+        return this.finalNumber;
+    }
+
+}
+
 class IntegerBinaryConverter {
     private String binaryNumber;
     public double finalNumber;
@@ -12,7 +59,7 @@ class IntegerBinaryConverter {
         this.binaryNumber = binaryNumber;
     }
 
-    public void reverseString() {
+    public void revertString() {
         String newNumber = new StringBuilder(this.binaryNumber).reverse().toString();
         setBinaryNumber(newNumber);
     }
@@ -37,12 +84,19 @@ public class BinaryConverter {
         System.out.println("Your binary number: ");
         String number = binaryNumber.nextLine();
 
-        // Reverts the number.
+        doubleBinaryConverter converter = new doubleBinaryConverter(number);
+        converter.revertString();
+        converter.splitNumber();
+        double finalNumber = converter.convertNumber();
+        System.out.println("Final number is: " + finalNumber);
+
+        /*
         IntegerBinaryConverter converter = new IntegerBinaryConverter(number);
-        converter.reverseString();
+        converter.revertString();
 
         double finalNumber = converter.toDecimal();
         System.out.println("Your binary number in decimal is: " + finalNumber);
+        */
 
         binaryNumber.close();
     }
