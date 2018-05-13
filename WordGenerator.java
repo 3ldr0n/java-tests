188f1dbd71cd665c5175ff54e399c8e84bf7f97c
Edison Neto <ednetoali@gmail.com>
2018-05-13 02:10:34 -0300

    new file:   LinkedList.java 	new file:   WordGenerator.java
    Created a type of LinkedList, and I'll build a word generator.

---
+++ WordGenerator.java
@@ -0,0 +1,8 @@
+public class WordGenerator {
+    public static void main(String[] args) {
+        char[] consonants = "bcdfghjklmnpqrstvxwyz".toCharArray();
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
+        System.out.println("" + consonants[5] + vowels[2]);
+    }
+}
